4bbdf9262c50dfb8280a847ea9c2e03b6a61546f
chrisachoo <mabs.chris@outlook.com>
2022-07-31 14:27:10 +0200

    initial commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const { signinUser, signupUser } = require('../controllers/userController')
+
+const router = express.Router()
+
+router.post('/signin', signinUser)
+router.post('/signup', signupUser)
+
+module.exports = router
