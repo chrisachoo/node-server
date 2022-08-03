dc1b587c27b48222b8f8e3dd65fb840b96e831f1
chrisachoo <mabs.chris@outlook.com>
2022-08-03 14:21:46 +0200

    procfile

--- serve.js
+++ serve.js
@@ -18,7 +18,7 @@ app.use('/api/user', userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 8080, () => {
       console.log('connected to db & listening on port', process.env.PORT)
     })
   })
